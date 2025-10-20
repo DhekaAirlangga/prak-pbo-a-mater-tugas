67ba1dd5e1d75dcb0689190cb1183edbf90f8d09
DhekaAirlangga <dhekaairlangga98@gmail.com>
2025-10-20 09:28:34 +0700

    p5

---
+++ p5/src/com/pert5/kuis/Animal.java
@@ -0,0 +1,21 @@
+package Praktikum.p5.src.com.pert5.kuis;
+
+public class Animal {
+    private String nama;
+    private int umur;
+
+    // Setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    };
+    public void setUmur(int umur) {
+        this.umur = umur;
+    };
+    // Getter
+    public String getNama() {
+        return this.nama;
+    };
+    public int getUmur() {
+        return this.umur;
+    };
+}
---
+++ p5/src/com/pert5/kuis/App.java
@@ -0,0 +1,12 @@
+package Praktikum.p5.src.com.pert5.kuis;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Animal animal = new Animal();
+
+        animal.setNama("Kucing");
+        animal.setUmur(3);
+
+        System.out.println("Kucing " + animal.getUmur() + " tahun " + "sedang bermain");
+    }
+}
